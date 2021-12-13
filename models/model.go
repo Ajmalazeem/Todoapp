1cceb9b30dc7113555a36f6e34a199b9cdf9ed94
Ajmal Aseem <ajmalaseem355@gmail.com>
2021-12-13 13:12:29 +0530

    bug fix

--- models/model.go
+++ models/model.go
@@ -10,7 +10,7 @@ type PostTodoRequest struct{
 	Id        string `json:"id" gorm:"coloumn:id"`
 	Todo string `json:"todo" gorm:"column:todo"`
 	CreatedAt time.Time `json:"created_at" gorm:"coloumn:created_at"`
-    UpdatedAt time.Time `gorm:"updated_at" "coloumn:updated_at"`
+    UpdatedAt time.Time `gorm:"updated_at" gorm:"coloumn:updated_at"`
     DeletedAt gorm.DeletedAt `gorm:"index"`
 }
 
